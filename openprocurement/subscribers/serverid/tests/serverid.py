d8df859a1b2d6a3583c76b7d3a3d17d023934fe9
Roman Kozlovskyi <krzroman@gmail.com>
2017-08-05 09:48:41 +0300

    Fixed processing exceptions by server_id subscriber

--- openprocurement/subscribers/serverid/tests/serverid.py
+++ openprocurement/subscribers/serverid/tests/serverid.py
@@ -12,6 +12,7 @@ from openprocurement.subscribers.serverid.serverid import (
 from datetime import datetime
 from pytz import timezone
 from pyramid.config import Configurator
+from pyramid.httpexceptions import HTTPNotFound, HTTPInternalServerError
 
 TZ = timezone(environ['TZ'] if 'TZ' in environ else 'Europe/Kiev')
 
@@ -22,6 +23,32 @@ def hello_world(request):
     return resp
 
 
+def status_404(request):
+    resp = request.response
+    resp.status = 404
+    resp.content_type = 'application/json'
+    return resp
+
+
+def status_500(request):
+    resp = request.response
+    resp.status = 500
+    resp.content_type = 'application/json'
+    return resp
+
+
+def error_404(request):
+    resp = HTTPNotFound(content_type='application/json')
+    resp.empty_body = True
+    return resp
+
+
+def error_500(request):
+    resp = HTTPInternalServerError(content_type='application/json')
+    resp.empty_body = True
+    return resp
+
+
 class SubscriberTest(unittest.TestCase):
 
     @classmethod
@@ -29,10 +56,21 @@ class SubscriberTest(unittest.TestCase):
         config = Configurator()
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
+        config.add_route('status_404', '/404')
+        config.add_view(status_404, route_name='status_404')
+        config.add_route('status_500', '/500')
+        config.add_view(status_500, route_name='status_500')
+        config.add_route('error_404', '/error404')
+        config.add_view(error_404, route_name='error_404')
+        config.add_route('error_500', '/error500')
+        config.add_view(error_500, route_name='error_500')
         config.registry.server_id = uuid.uuid4().hex
         includeme(config)
         app = config.make_wsgi_app()
         cls.app = webtest.TestApp(app)
+
+    def tearDown(self):
+        self.app.reset()
 
     def test_get_time(self):
         time = get_time()
@@ -62,14 +100,14 @@ class SubscriberTest(unittest.TestCase):
         config.registry.server_id = ''
         self.assertEqual(config.registry.server_id, '')
         includeme(config)
-        self.assertNotEqual(config.registry.server_id, '')
-        self.assertEqual(len(config.registry.server_id), 32)
+        self.assertNotEqual(config.registry.couchdb_server_id, '')
+        self.assertEqual(len(config.registry.couchdb_server_id), 32)
 
-    def test_couch_uuid_validator(self):
+    def _test_couch_uuid_validator(self, path, status_code, status):
         # Request without cookie SERVER_ID
         self.assertEqual(self.app.cookies, {})
-        resp = self.app.get('/')
-        self.assertEqual(resp.status, '200 OK')
+        resp = self.app.get(path, status=status_code)
+        self.assertEqual(resp.status, status)
         self.assertEqual(resp.body, '')
         header = resp.headers.get('Set-Cookie', None).split(';')[0].split('=')
         header_name = header[0]
@@ -80,7 +118,7 @@ class SubscriberTest(unittest.TestCase):
         # Request POST without cookie SERVER_ID
         self.app.reset()
         self.assertEqual(self.app.cookies, {})
-        resp = self.app.post_json('/', {'data': 'test data'}, status=412)
+        resp = self.app.post_json(path, {'data': 'test data'}, status=412)
         self.assertEqual(resp.status, '412 Precondition Failed')
         header = resp.headers.get('Set-Cookie', None).split(';')[0].split('=')
         header_name = header[0]
@@ -91,8 +129,8 @@ class SubscriberTest(unittest.TestCase):
         # Request with valid cookie SERVER_ID
         cookie = self.app.cookies.get('SERVER_ID', None)
         self.assertNotEqual(cookie, None)
-        resp = self.app.get('/')
-        self.assertEqual(resp.status, '200 OK')
+        resp = self.app.get(path, status=status_code)
+        self.assertEqual(resp.status, status)
         self.assertEqual(resp.body, '')
         header = resp.headers.get('Set-Cookie', None)
         self.assertEqual(header, None)
@@ -100,7 +138,7 @@ class SubscriberTest(unittest.TestCase):
         # Request with invalid cookie SERVER_ID
         cookie_value = 'f2154s5adf2as1f54asdf1as56f46asf3das4f654as31f456'
         self.app.set_cookie('SERVER_ID', cookie_value)
-        resp = self.app.get('/', status=412)
+        resp = self.app.get(path, status=412)
         self.assertEqual(resp.status, '412 Precondition Failed')
         self.assertEqual(resp.request.cookies.get('SERVER_ID', None), cookie_value)
         header = resp.headers.get('Set-Cookie', None).split(';')[0].split('=')
@@ -108,6 +146,21 @@ class SubscriberTest(unittest.TestCase):
         header_value = header[1]
         self.assertEqual(header_name, 'SERVER_ID')
         self.assertEqual(len(header_value), 128)
+
+    def test_couch_uuid_validator(self):
+        self._test_couch_uuid_validator('/', 200, '200 OK')
+
+    def test_couch_uuid_validator_404(self):
+        self._test_couch_uuid_validator('/404', 404, '404 Not Found')
+
+    def test_couch_uuid_validator_500(self):
+        self._test_couch_uuid_validator('/500', 500, '500 Internal Server Error')
+
+    def test_couch_uuid_validator_error404(self):
+        self._test_couch_uuid_validator('/error404', 404, '404 Not Found')
+
+    def test_couch_uuid_validator_error500(self):
+        self._test_couch_uuid_validator('/error500', 500, '500 Internal Server Error')
 
 
 def suite():
